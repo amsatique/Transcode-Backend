7dfb396afb388873c7a12b31989d2158ae11868d
amsatique <amsatique@gmail.com>
2016-01-20 22:17:14 +0100

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ git clone https://github.com/amsatique/hapi-struct.git
 cd /hapi-struct
 
 #Add Hapi-struct config
-sed -i -e 's/"db": "YourDataBaseName"/"db": $MONGODB_NAME/g' /Hapi-Struct/config/parameters.json
-sed -i -e 's/"username": "YourDataBaseUserName"/"username": $MONGODB_USER/g' /Hapi-Struct/config/parameters.json
-sed -i -e 's/"password": "YourDatabasePassword"/"password": $MONGODB_PASS/g' /Hapi-Struct/config/parameters.json
-sed -i -e 's/"privateKey": "YourPrivateKey"/"privateKey": $PRIVATE_KEY/g' /Hapi-Struct/config/parameters.json
-sed -i -e 's/"email": "senderMail@website.com"/"email": $EMAIL/g' /Hapi-Struct/config/parameters.json
-sed -i -e 's/"userName": "YourMailAccount"/"username": $EMAIL_NAME/g' /Hapi-Struct/config/parameters.json
-sed -i -e 's/"password": "YourMailPassword"/"password": $EMAIL_PASSWD/g' /Hapi-Struct/config/parameters.json
+sed -i -e 's/"db": "YourDataBaseName"/"db": $MONGODB_NAME/g' /hapi-struct/config/parameters.json
+sed -i -e 's/"username": "YourDataBaseUserName"/"username": $MONGODB_USER/g' /hapi-struct/config/parameters.json
+sed -i -e 's/"password": "YourDatabasePassword"/"password": $MONGODB_PASS/g' /hapi-struct/config/parameters.json
+sed -i -e 's/"privateKey": "YourPrivateKey"/"privateKey": $PRIVATE_KEY/g' /hapi-struct/config/parameters.json
+sed -i -e 's/"email": "senderMail@website.com"/"email": $EMAIL/g' /hapi-struct/config/parameters.json
+sed -i -e 's/"userName": "YourMailAccount"/"username": $EMAIL_NAME/g' /hapi-struct/config/parameters.json
+sed -i -e 's/"password": "YourMailPassword"/"password": $EMAIL_PASSWD/g' /hapi-struct/config/parameters.json
 
 npm install
 
